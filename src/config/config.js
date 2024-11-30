aae21ecc42b5d0b66c139fff40ed7f4cb5d78cab
Terence <terencebc92@gmail.com>
2024-11-30 21:33:27 +0800

    change config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 // const baseUrl = "https://my-employee-app-be.azurewebsites.net";
-const baseUrl = "http://localhost:8083";
+// const baseUrl = "http://localhost:8083";
+const baseUrl = "http://192.168.1.109:8083";
+
 const employeesUrl = `${baseUrl}/api/v1/employees`;
 
 export { baseUrl, employeesUrl };
