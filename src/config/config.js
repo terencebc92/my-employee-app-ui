021fbacfd51bc7f1e058f864012dd1abed5a9600
Terence <terencebc92@gmail.com>
2024-12-01 21:00:02 +0800

    update urls

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // const baseUrl = "https://my-employee-app-be.azurewebsites.net";
 // const baseUrl = "http://192.168.1.109:8083";
-const baseUrl = "http://terence-app-be.duckdns.org";
+const baseUrl = "https://terence-app-be.duckdns.org";
 
 const employeesUrl = `${baseUrl}/api/v1/employees`;
 
